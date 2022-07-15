bb30930d3f1b9e119500774d2dd2e13dc3e2de0a
junjunruru <junjun199603@gmail.com>
2022-07-15 14:56:06 +0800

    測試

--- Spark/spark.py
+++ Spark/spark.py
@@ -11,7 +11,7 @@ df = df.union(spark.read.csv('H_lvr_land_A.csv', header=True, inferSchema=True))
 #篩選資料
 df = df.filter(df['主要用途'].contains('住家用')).filter(df['建物型態'].contains('住宅大樓')).filter(~df['總樓層數'].isin(['一層','二層','三層','四層','五層','六層','七層','八層','九層','十層','十一層','十二層']))
 df = df.sort(desc('交易年月日'))
-
+print(df.count())
 # def digital_conversion(n): 將中文轉換數字
 #     number_chinese = {
 #         '一': 1,
@@ -39,3 +39,4 @@ df = df.sort(desc('交易年月日'))
 
 # df = df.toPandas() 轉成pandas 輸出json但格式不對
 # df.to_json('aa.json', orient='table', force_ascii=False)
+
