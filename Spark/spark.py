a308f1e7f303e1d341e6b1287e1014bf4b5a7f3b
junjunruru <junjun199603@gmail.com>
2022-07-18 01:16:42 +0800

    修

--- Spark/spark.py
+++ Spark/spark.py
@@ -31,7 +31,8 @@ json_data = ['result-part1.json' , 'result-part2.json']
 for i in city:
     with open(random.choice(json_data), 'a',encoding='utf-8') as f:
         f.write(city[i])
-        f.write(',')
+        # f.write(',')
+        f.write('\n')
 
 
 
